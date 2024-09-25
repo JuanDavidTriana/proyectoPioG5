243373cea4a7de2e7a0166b53eaa2527351033ab
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2024-09-25 16:48:23 -0500

    Update ejercicio3.py

--- 6. Funciones/Ejercicios/ejercicio3.py
+++ 6. Funciones/Ejercicios/ejercicio3.py
@@ -1,31 +1,45 @@
 def agregar_producto(carrito, producto, precio):
-    carrito.append({"producto": producto, "precio": precio}) 
+    carrito.append({"producto": producto, "precio": precio})
+    print("Producto agregado exitosamente...") 
 
-car = []  
+def mostar_carrito(carrito):
+    if carrito:  
+        for item in carrito:
+            print(f"Producto: {item['producto']}, Precio: {item['precio']}")  
+    else:
+        print("El carrito está vacío.")
+
+def calcular_total(carrito):
+    return sum(item['precio'] for item in carrito)
+    
+def menu_opciones():
+    print("-------------Menu-------------")
+    print("\n1. Agregar producto")
+    print("2. Ver carrito")
+    print("3. Calcular total")
+    print("4. Salir")
+
+#Carrito 
+carrito = []  
 
 while True:
-    print("\n1. Agregar")
-    print("2. Ver")
-    print("3. Total")
-    print("4. Salir")
-    opt = input("Elige: ")
+    menu_opciones()
+    opcion = input("Elije una opcion: ")
     
-    if opt == "1":
-        prod = input("Nombre: ")
-        precio = float(input("Precio: "))  
-        agregar_producto(car, prod, precio) 
-    elif opt == "2":
-        if len(car) > 0:  
-            for i in range(len(car)):
-                print("Producto: " + car[i]['producto'] + ", Precio: " + str(car[i]['precio']))  # Imprimir productos
-        else:
-            print("El carrito está vacío.")
-    elif opt == "3":
-        total = 0
-        for i in range(len(car)):  
-            total += car[i]['precio']
-        print("Total: " + str(total)) 
-    elif opt == "4":
+    if opcion == "1":
+        producto = input("Nombre del producto: ")
+        precio = float(input("Precio del producto: "))  
+        agregar_producto(carrito, producto, precio) 
+        
+    elif opcion == "2":
+        mostar_carrito(carrito)
+        
+    elif opcion == "3":
+        total = calcular_total(carrito)
+        print(f"Total del carrito es de : ${total}")   
+        
+    elif opcion == "4":
+        print("Saliendo....")
         break
     else:
         print("Opción incorrecta") 
